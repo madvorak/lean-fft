cdc40f9d90b9b57e28b67115914b9301e76b9895
madvorak <dvorakmartinbridge@seznam.cz>
2023-04-13 18:49:11 +0200

    refactoring

--- NumberTheoreticTransform.lean
+++ NumberTheoreticTransform.lean
@@ -7,13 +7,18 @@ import Mathlib.Tactic.Cases
 namespace Matrix
 
 
+--  level n = 2 ^ n
+def level : ℕ -> ℕ
+| 0   => 1
+| n+1 => level n + level n
+
 def e : ℕ := 4
-def M : ℕ := 2^e + 1
-def vektor : Type := Fin (2^e) → ZMod M
+def M : ℕ := level e + 1
+def vektor : Type := Fin (level e) → ZMod M
 
 
-def transform (t : ℕ) (ω : ZMod M) (x : Fin (2^t) → ZMod M) : (Fin (2^t) → ZMod M) :=
-fun (i : Fin (2^t)) => dotProduct (fun (j : Fin (2^t)) => ω ^ ((i : ℕ) * (j : ℕ))) x
+def transform (t : ℕ) (ω : ZMod M) (x : Fin (level t) → ZMod M) : (Fin (level t) → ZMod M) :=
+fun (i : Fin (level t)) => dotProduct (fun (j : Fin (level t)) => ω ^ (i.val * j.val)) x
 
 def NTS : vektor → vektor := transform e 3
 
@@ -23,47 +28,32 @@ fun i => - (x i)
 def NTT : vektor → vektor := negate ∘ transform e 6
 
 
-lemma index_ok {t : ℕ} (i : Fin (2 ^ t)) : 2 * (i : ℕ) + 1 < 2 ^ t.succ := by
-  show 2 * (i : ℕ) + 2 ≤ 2 ^ t.succ
-  convert_to 2 * ((i : ℕ) + 1) ≤ 2 * 2 ^ t
-  · rw [pow_succ]
-  apply LinearOrderedCommMonoid.mul_le_mul_left
-  exact Fin.is_lt i
+lemma index_ok {t : ℕ} (i : Fin (level t)) : 2 * (i : ℕ) + 1 < level t.succ := by
+  unfold level
+  have hi := Fin.is_lt i
+  linarith
 
-def splitter {t : ℕ} (x : Fin (2 ^ t.succ) → ZMod M) :
-  (Fin (2 ^ t) → ZMod M) × (Fin (2 ^ t) → ZMod M) :=
+def splitter {t : ℕ} (x : Fin (level (t+1)) → ZMod M) :
+  (Fin (level t) → ZMod M) × (Fin (level t) → ZMod M) :=
 Prod.mk
-  (fun (i : Fin (2 ^ t)) => x ⟨2 * (i : ℕ), Nat.lt_of_succ_lt (index_ok i)⟩)
-  (fun (i : Fin (2 ^ t)) => x ⟨2 * (i : ℕ) + 1, index_ok i⟩)
+  (fun (i : Fin (level t)) => x ⟨2 * (i : ℕ), Nat.lt_of_succ_lt (index_ok i)⟩)
+  (fun (i : Fin (level t)) => x ⟨2 * (i : ℕ) + 1, index_ok i⟩)
 
-def transform_fast (t : ℕ) (ω : ZMod M) (x : Fin (2^t) → ZMod M) : (Fin (2^t) → ZMod M) :=
+def transform_fast (t : ℕ) (ω : ZMod M) (x : Fin (level t) → ZMod M) : (Fin (level t) → ZMod M) :=
 match t with
 | 0   => x
 | n+1 =>
   let p := splitter x
   let a := transform_fast n (ω * ω) p.1
   let b := transform_fast n (ω * ω) p.2
-  fun (i : Fin (2 ^ (n+1))) =>
-    let j : ℕ := i
-    if hj : (j : ℕ) < 2 ^ n
-    then
-      let J : Fin (2 ^ n) := ⟨j, hj⟩
-      a J + ω ^ j * b J
-    else
-      let j' : ℕ := j - 2 ^ n
-      let J' : Fin (2 ^ n) := ⟨j', by {
-        push_neg at hj
-        have hh : j < 2 ^ (n + 1)
-        · exact Fin.is_lt i
-        rw [pow_succ, two_mul, ←tsub_lt_iff_right hj] at hh
-        exact hh
-      }⟩
-      a J' - ω ^ j' * b J'
+  Fin.append
+    (fun j => a j + ω ^ j.val * b j)
+    (fun j => a j - ω ^ j.val * b j)
 
 def FNTS : vektor → vektor := transform_fast e 3
 def FNTT : vektor → vektor := negate ∘ transform_fast e 6
 
-
+/-
 #eval  NTS ![4, 4, 4, 1, 1, 5, 5, 5, 5, 5, 7, 7, 7, 0, 0, 9]
 #eval FNTS ![4, 4, 4, 1, 1, 5, 5, 5, 5, 5, 7, 7, 7, 0, 0, 9]
 #eval  NTS ![4, 4, 4, 1, 1, 5, 5, 5, 5, 7, 7, 7, 7, 0, 0, 9]
@@ -72,7 +62,7 @@ def FNTT : vektor → vektor := negate ∘ transform_fast e 6
 #eval FNTT ![1, 6, 8, 10, 16, 15, 14, 6, 14, 0, 14, 8, 3, 3, 2, 12]
 #eval  NTT ![3, 0, 9, 7, 8, 5, 10, 1, 12, 6, 13, 11, 11, 13, 6, 0]
 #eval FNTT ![3, 0, 9, 7, 8, 5, 10, 1, 12, 6, 13, 11, 11, 13, 6, 0]
-
+-/
 
 theorem transform_fast_correct : transform = transform_fast := by
   --ext t ω x j
@@ -80,23 +70,27 @@ theorem transform_fast_correct : transform = transform_fast := by
   intro t
   induction' t with n ih
   · ext ω x j
-    have j_zero : j = 0
-    · cases' j with v proo
-      have value_zero : v = 0
-      · exact Iff.mp Nat.lt_one_iff proo
-      exact Fin.ext value_zero
-    rw [j_zero]
-    unfold transform transform_fast dotProduct
+    have jz : j = (0 : Fin 1)
+    · exact Fin.ext (Fin.coe_fin_one j)
+    rw [jz]
+    unfold transform transform_fast dotProduct Finset.univ Fintype.elems Fin.fintype
     simp
+    rfl
   ext ω x j
   unfold transform_fast
   rw [←ih]
-  by_cases hj : (j : ℕ) < 2^n
-  · simp [hj]
-    unfold splitter
-    simp
-    convert_to
-      transform (Nat.succ n) ω x j =
-      _ + _
+  by_cases hj : j.val < level n
+  · let J : Fin (level n) := ⟨j.val, hj⟩
+    --let c := Fin.castAdd (level n) j
+    convert_to transform (Nat.succ n) ω x j =
+      let p := splitter x
+      let a := transform n (ω * ω) p.fst
+      let b := transform n (ω * ω) p.snd
+      (fun j => a j + ω ^ j.val * b j) J
+    · simp --simp_rw [←Fin.append_left]
+      sorry
+    show transform (Nat.succ n) ω x j =
+      let p := splitter x
+      transform n (ω * ω) p.fst J + ω ^ j.val * transform n (ω * ω) p.snd J
     sorry
-  sorry
+  · sorry
